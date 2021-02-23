824dc3814a47ac72e0aa89c933b58d319c09af1c
Ekaterina Klimenko <klimenko3007@gmail.com>
2021-02-23 10:59:40 +0100

    added checkboxes and made them work

--- code/script.js
+++ code/script.js
@@ -3,13 +3,15 @@ const recipe = document.getElementById('category-wrapper')
 //const currentRecipe = document.getElementById('current-recipe-container')
 const form = document.getElementById('form')
 const recipesContainer = document.getElementById('recipes-container')
+const allCheckboxes = document.querySelectorAll('.checkbox')
 
 let recipeArrayFiltered;
 
 //Functions that can only be run within fetch
-const filterByIngredients = () => {
+const filterByIngredients = () => {   //make sure filters can be applied in any order... but wait... maybe this is a non-problem? because the order shouldn't matter?
+    const checkbox = document.getElementById('checkbox').value
     recipeArrayFiltered = recipeArray.filter ((element) =>{
-        return element.ingredientLines.length <= 5; 
+        return element.ingredientLines.length <= Number(checkbox); 
   });
 }
 const filterByHealthLable = () =>{
@@ -18,16 +20,16 @@ const filterByHealthLable = () =>{
       })
 }
 const start = (userInput) => {
-  const API = `https://api.edamam.com/search?q=${userInput}&app_key=6aec21ec4b520e9694efcadc0c641e29&app_id=94e5ef74&from=0&to=20`
+    recipesContainer.innerHTML = "";
+    const API = `https://api.edamam.com/search?q=${userInput}&app_key=6aec21ec4b520e9694efcadc0c641e29&app_id=94e5ef74&from=0&to=20`
   fetch(API)
     .then((response) => {
       return response.json()
     })
     .then((json) => {
-      
       const recipeArray = Array.from(
         json.hits, element => element.recipe
-      );
+      ); 
       recipeArrayFiltered = recipeArray;
       
       const recipeTitleArray = Array.from(
@@ -55,20 +57,21 @@ const start = (userInput) => {
           `
         })
       })
-      
-      
-      // currentRecipe.innerHTML = `<img class="recipe-img" src="${json.hits[0].recipe.image}" alt="picture">
-      //   <p class="recipe-name">${json.hits[0].recipe.label}</p>
-      //   `
-      // json.hits[0].recipe.healthLabels.forEach((element) => {
-      //   currentRecipe.innerHTML += `<span class="recipe-health">${element}</span>, `
-      // })
     })
 
 }
+
+//Event Listeners
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
+  event.preventDefault()
   const textInput = document.getElementById('text-input').value;
   start(textInput)
+})
 
+allCheckboxes.forEach((checkbox)=>{
+    checkbox.addEventListener('change', ()=>{
+        const checkboxValue = checkbox.value
+        console.log(checkboxValue)
+        start(checkboxValue)
+    })
 })
